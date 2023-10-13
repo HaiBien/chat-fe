dda41506bf9a566830aa859619f2df112ad95532
lvh.bien@gmail.com <lvh.bien@gmail.com>
2023-10-13 09:20:58 +0700

    ########

--- src/App.js
+++ src/App.js
@@ -1,21 +1,7 @@
 import './App.css';
-import FacebookLogin from 'react-facebook-login';
 import FbLogin from './FbLogin';
 
 function App() {
-
-  const responseFacebook = (response) => {
-    if (response) {
-      console.log('response', response)
-    }
-  };
-
-  const handleFacebookLogin = () => {
-    console.log('document.getElementById("msgbtn")', document.getElementById("msgbtn"))
-    document.getElementById("msgbtn").click();
-    let cl = document.getElementsByClassName("msgbtn");
-    console.log('cl', cl)
-  };
 
   return (
     <div className="App">
