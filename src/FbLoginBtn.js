d5d7562d74dfa7ea48f483deea6c8c426b5e46eb
lvh.bien@gmail.com <lvh.bien@gmail.com>
2023-10-16 09:21:54 +0700

    2222

---
+++ src/FbLoginBtn.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import FacebookLogin from 'react-facebook-login';
+
+function App() {
+  const responseFacebook = (response) => {
+    console.log('Thông tin đăng nhâp FB: ', +response);
+  };
+
+  return (
+    <div>
+      <FacebookLogin
+        appId="291801013674575"
+        autoLoad={false}
+        fields="name,email,picture"
+        callback={responseFacebook}
+      />
+    </div>
+  );
+}
+
+export default App;
