f720ebf1ce579c1bd54fd085d8cfbab68cf6df15
Hai Bien <lvh.bien@gmail.com>
2024-02-15 16:45:36 +0700

    #36

--- src/LoginFacebook.js
+++ src/LoginFacebook.js
@@ -15,7 +15,8 @@ const App = () => {
   };
 
   const fetchFanpages = async (token = userAccessToken) => {
-    const response = await fetch(`https://graph.facebook.com/v18.0/me/accounts?fields=name,access_token,picture&access_token=${token}`);
+    // const response = await fetch(`https://graph.facebook.com/v18.0/me/accounts?fields=name,access_token,picture&access_token=${token}`);
+    const response = await fetch(`https://graph.facebook.com/v18.0/me/accounts?access_token=${token}`);
     const data = await response.json();
     console.log('pages', data.data)
     setFanpages(data.data);
