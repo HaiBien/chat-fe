5ad31d0ce780b588c41d5782aa7555d991018b7e
Hai Bien <lvh.bien@gmail.com>
2024-07-25 14:32:13 +0700

    #46

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
 import LoginFacebook from './LoginFacebook';
 import Avatar from './Avatar'
+import MicrosoftLogin from "react-microsoft-login";
 
 function App() {
-
+    const authHandler = (err, data) => {
+        console.log(err, data);
+    };
     return (
         <div className="App">
             <LoginFacebook/>
+            <MicrosoftLogin
+                clientId="dee41533-cfca-4aff-8f47-3a279aaf4adb"
+                authCallback={authHandler}
+            />
             <Avatar name=" Hải Bien"/>
             <Avatar name="le bin HO"/>
             <img src={"https://platform-lookaside.fbsbx.com/platform/profilepic/?asid=122121079694140706&height=50&width=50&ext=1712219031&hash=AfrOTFzCOJchVDCZEL6ewOFE3887TwNqQ1SPrJ1tUdXzRw"} />
