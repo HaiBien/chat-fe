f0f77bd3ced9b61c597e875cf5121fbc64364f48
Hai Bien <lvh.bien@gmail.com>
2024-07-26 11:44:01 +0700

    #48

--- src/MicrosoftSSO/authConfig.js
+++ src/MicrosoftSSO/authConfig.js
@@ -5,7 +5,7 @@ const msalConfig = {
     auth: {
         clientId: "dee41533-cfca-4aff-8f47-3a279aaf4adb",
         authority: "https://login.microsoftonline.com/cd92bb82-dddf-47a8-a155-02851486fd74",
-        redirectUri: "https://chat-fe-x30q.onrender.com",
+        redirectUri: "https://auth.optisigns.com/__/auth/handler",
     },
     cache: {
         cacheLocation: "localStorage", // Đây là tùy chọn, bạn có thể dùng "sessionStorage"
